dd31e3a4f9529e6ffba297315bd3ef3876e57077
Dutkercz <dutkercz@gmail.com>
2025-04-22 17:16:18 -0300

    Desenv. Entidade Trabalhador

---
+++ src/main/java/dutkercz/com/github/flash_freela/entities/Trabalhador.java
@@ -0,0 +1,31 @@
+package dutkercz.com.github.flash_freela.entities;
+
+import dutkercz.com.github.flash_freela.entities.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table
+public class Trabalhador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private LocalDate nascimento;
+    private Double avaliacao;
+    private Integer totalDeTrabalhos;
+    private Endereco endereco;
+    private Role role;
+}
